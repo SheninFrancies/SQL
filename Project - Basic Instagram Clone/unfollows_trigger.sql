075c36e00bdab0fd339242561e9cc41c1c05af16
SheninFrancies <117643956+SheninFrancies@users.noreply.github.com>
2023-03-29 12:58:40 +0300

    Instagram Clone Project

---
+++ Project - Basic Instagram Clone/unfollows_trigger.sql
@@ -0,0 +1,12 @@
+-- Trigger to prevent users to follow themselves
+
+DELIMITER $$
+
+CREATE TRIGGER capture_unfollow
+	AFTER DELETE ON follows FOR EACH ROW
+    BEGIN
+		INSERT INTO unfollows
+        SET follower_id = OLD.follower_id, 
+			followee_id = OLD.followee_id;
+    END;
+$$
---
+++ Project - Basic Instagram Clone/users_trigger.sql
@@ -0,0 +1,4 @@
+USE trigger_database;
+SHOW TABLES;
+INSERT INTO users(username, age) VALUES ('Yang', 12);
+SELECT * FROM users;
